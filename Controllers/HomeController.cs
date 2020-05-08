505cdb4fdf040782f3ea74fc810d7548e3003482
owner00 <37192287+owner00@users.noreply.github.com>
2020-05-08 21:40:14 +0100

    Upload project resources
    Sending the project files and resources required to build the ASP.NET core webapp.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SquareRootComparer.Models;
+
+namespace SquareRootComparer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string firstNumber, string secondNumber)
+        {
+            int firstNo = int.Parse(firstNumber);
+            int secondNo = int.Parse(secondNumber);
+            ViewBag.FirstNumber = firstNo;
+            ViewBag.SecondNumber = secondNo;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
